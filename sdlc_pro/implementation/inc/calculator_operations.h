32c1a6185b33c6d5a77dd59818644af5a5095d59
99002642 <71489688+99002642@users.noreply.github.com>
2020-09-19 12:30:16 +0530

    Update calculator_operations.h

--- sdlc_pro/implementation/inc/calculator_operations.h
+++ sdlc_pro/implementation/inc/calculator_operations.h
@@ -21,7 +21,7 @@ int oddnum(int operand1);
 int percent(int operand1, int operand2);
 int fact(int operand1);
 float area_p(float operand1, float operand2);
-int volumesp(int operand1, int operand2); 
+
 
 
 
