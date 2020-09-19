baebbe6a7d39a309fa16639cf796c28814cff4c8
99002642 <71489688+99002642@users.noreply.github.com>
2020-09-19 10:53:34 +0530

    Update test_calculator_operations.c

--- sdlc_pro/implementation/test/test_calculator_operations.c
+++ sdlc_pro/implementation/test/test_calculator_operations.c
@@ -19,6 +19,7 @@ void test_evennum(void);
 void test_oddnum(void);
 void test_percent(void);
 void test_fact(void);
+void test_fibo(void);
 /* Start of the application test */
 int main() {
 /* Note: Do not edit START*/
@@ -43,6 +44,7 @@ int main() {
   CU_add_test(suite, "oddnum", test_oddnum);
   CU_add_test(suite, "percent", test_percent);
   CU_add_test(suite, "fact", test_fact);
+  CU_add_test(suite, "fibo", test_fibo);
 
 
 /* Note: Do not edit START*/
