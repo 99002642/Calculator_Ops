9bceb0ae95f2b9bccb49619fb600051eeee1e7fe
99002642 <71489688+99002642@users.noreply.github.com>
2020-09-19 11:45:06 +0530

    Update test_calculator_operations.c

--- sdlc_pro/implementation/test/test_calculator_operations.c
+++ sdlc_pro/implementation/test/test_calculator_operations.c
@@ -138,7 +138,7 @@ void test_area_p(void)
 }
 void test_vol_sp(void)
 {
-  CU_ASSERT(463433.14285756 == vol_sp((4.0/3.0)*3.14285714286*(48*48*48)));
+  CU_ASSERT(442368 == vol_sp((4.0/3.0)*3*(48*48*48)));
 }
             
                                       
